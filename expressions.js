fb03c6774e1b5c75f8bf5d7234952fe63aeea9a6
Eva Larumbe <evalarumbe@gmail.com>
2019-08-30 15:58:09 +0200

    Add examples and explanation of expressions

---
+++ expressions.js
@@ -0,0 +1,31 @@
+// this is an expression. it doesn't tell JS to do anything.
+// it can be evaluated to something else.
+// 'hallo'
+
+// for more examples of expressions, see the docs:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#Expressions
+
+// this is a statement. it tells JS to 'do something'
+// this statement in particular (the console's log method)
+// accepts an expression as a parameter
+// this method will evaluate expressions to a string,
+// so that JS knows which characters should be printed to the terminal
+console.log('blah');
+
+// function greet(person) {
+
+//     // if statement conditions must also be expressions
+//     // in this case, the expression will evaluate to a boolean,
+//     // so that JS knows which blocks of code should run
+//     if (person === 'pirate') {
+//         return 'ahoy';
+//     } else if (person === 'aussie') {
+//         return 'oi';
+//     } else {
+//         return 'hi, who are you?';
+//     }
+// }
+
+// // console.log(greet('pirate'));
+// console.log(greet('aussie'));
+// console.log(greet('student'));
