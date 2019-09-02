221f4f21929031e422003fe03e1ebe9f01758ce9
Eva Larumbe <evalarumbe@gmail.com>
2019-09-02 17:43:13 +0200

    Add square bracket notation examples for arrays and strings

---
+++ arrays-strings-square-brackets.js
@@ -0,0 +1,15 @@
+const mySentence = ['hey', 'here', 'is', 'a', 'sentence', 'in', 'parts'];
+// (console.log will evaluate expressions to strings)
+// console.log(mySentence[4]);
+
+// strings have some similarities with arrays
+// both can access elements by index
+// some of their built-in methods are different
+
+const myColor = 'purple';
+
+console.log(myColor);
+
+// two different ways to access the element at index 2 of a string
+console.log(myColor[2]);
+console.log(myColor.charAt(2));
