d2ee114a4aeb4c173034a61022f183131d43216d
Eva Larumbe <evalarumbe@gmail.com>
2019-09-02 11:18:28 +0200

    Add examples: function reference vs function call

--- func-expressions.js
+++ func-expressions.js
@@ -1,19 +1,22 @@
 // There are different ways to define a function
 
 // One way: function declaration syntax. It begins with the 'function' keyword
-function greet(person) {
-    if (person === 'pirate') {
-        return 'ahoy';
-    } else if (person === 'aussie') {
-        return 'oi';
-    } else {
-        return 'hi, who are you?';
-    }
-}
+// function greet(person) {
+//     if (person === 'pirate') {
+//         return 'ahoy';
+//     } else if (person === 'aussie') {
+//         return 'oi';
+//     } else {
+//         return 'hi, who are you?';
+//     }
+// }
 
 // Another way: function expression syntax
 // Assigns an anonymous (nameless) function to a variable
-const sayGreeting = function(person) {
+let sayGreeting = 'Heyo';
+console.log(sayGreeting);
+
+sayGreeting = function(person) {
     if (person === 'pirate') {
         return 'ahoy';
     } else if (person === 'aussie') {
@@ -22,8 +25,10 @@ const sayGreeting = function(person) {
         return 'hi, who are you?';
     }
 }
-
-console.log(greet('pirate'));
-
+// function reference (Here's a function, so call me maybe)
+console.log(sayGreeting);
+// function call (will be replaced by a return value if there is one)
 console.log(sayGreeting('aussie'));
+
+// console.log(greet('pirate'));
 // console.log(greet('student'));
