36103606d67bbddcebecea5927a9a6293ddb336b
Eva Larumbe <evalarumbe@gmail.com>
2019-09-03 10:25:54 +0200

    Refactor to use a callback

--- func-expressions.js
+++ func-expressions.js
@@ -1,27 +1,12 @@
 const talkPirate = function() { return 'ahoy'; };
 const greetAnAustralian = function() { return 'oi'; };
 
-// When we pass a function reference, it's called a CALLBACK
-// But here we are grabbing the return value of talkPirate().
-// (talkToJenna is not able to call the argument)  
-console.log(talkToJenna(talkPirate()));
-// the above line is the same as
-// console.log(talkToJenna('ahoy'));
-
-function talkToJenna(howToTalk) { // I'm expecting something like 'ahoy'
+function talkToJenna(howToTalk) {
     let greeting = '';
 
-    greeting = `${howToTalk} Jenna!`
+    greeting = `${howToTalk()} Jenna!`
 
     return greeting;
 }
 
-/**
- * talkPirate
- * is a function reference. This gets replaced by
- * function() { return 'ahoy'; }
- * 
- * talkPirate()
- * is a function call. This gets replaced by
- * 'ahoy'
- */
+console.log(talkToJenna(talkPirate));
