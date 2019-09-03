97afad3a0b96d471e27d6b46720988536489fd46
Eva Larumbe <evalarumbe@gmail.com>
2019-09-03 12:53:17 +0200

    Refactor to condense lines of code

--- func-expressions.js
+++ func-expressions.js
@@ -2,11 +2,20 @@ const talkPirate = function() { return 'ahoy'; };
 const greetAnAustralian = function() { return 'oi'; };
 
 function talkToJenna(howToTalk) {
-    let greeting = '';
-
-    greeting = `${howToTalk()} Jenna!`
-
-    return greeting;
+    return `${howToTalk()} Jenna!`;
 }
 
 console.log(talkToJenna(talkPirate));
+
+// Here's another way to write the same function,
+// using the accumulator pattern.
+// Breaking down the steps like this will be useful
+// as your funcs become more complex
+
+// function talkToJenna(howToTalk) {
+//     let greeting = '';
+
+//     greeting = `${howToTalk()} Jenna!`;
+
+//     return greeting;
+// }
