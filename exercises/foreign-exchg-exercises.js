03d8d808c7a41571b25bfb4e4d225ea6a23268b8
Eva Larumbe <evalarumbe@gmail.com>
2019-09-05 11:11:01 +0200

    Add foreign exchange exercise 3

--- exercises/foreign-exchg-exercises.js
+++ exercises/foreign-exchg-exercises.js
@@ -10,6 +10,19 @@
 // (Note: this is just an exercise. In the real world, aim for
 // just one function syntax per codebase and keep it consistent)
 
+// 3. Refactor to give a result to two decimal places, like this:
+
+// 145.089 should round to 145.09
+// 145.085 should round to 145.09
+// 145.082 should round to 145.08
+
+// Note: Because JS is weird, add some code that converts
+// floating points values to an integer that represents cents
+// and perform arithmetic on integers only
+// Once you have the result in cents, convert it back to decimal
+// as the last step just before returning it
+// Further reading: http://adripofjavascript.com/blog/drips/avoiding-problems-with-decimal-math-in-javascript.html
+
 function convertFromEur(amount, curr) {
   switch (curr) {
     case /* ??? */:
