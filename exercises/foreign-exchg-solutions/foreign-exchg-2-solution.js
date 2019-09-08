bd66dd93704768efbcb25d1af8603e618b52f3ff
Eva Larumbe <evalarumbe@gmail.com>
2019-09-08 16:43:45 +0200

    Add semicolons

--- exercises/foreign-exchg-solutions/foreign-exchg-2-solution.js
+++ exercises/foreign-exchg-solutions/foreign-exchg-2-solution.js
@@ -23,14 +23,14 @@ const getAudFromEur = function(amount) {
   // 1 EUR = 1.62072 AUD
   const rate = 1.62072;
   return amount * rate;
-}
+};
 
 // arrow function expression syntax (new in ES6) 
 const getChfFromEur = (amount) => {
   // 1 EUR = 1.08329 CHF
   const rate = 1.08329;
   return amount * rate;
-}
+};
 
 console.log(convertFromEur(100, 'AUD'));
 console.log(convertFromEur(200, 'JPY'));
