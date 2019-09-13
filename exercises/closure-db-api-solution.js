06fa25936b5151c1601caca1de104d3faaf80bac
Eva Larumbe <evalarumbe@gmail.com>
2019-09-13 16:43:06 +0200

    Closure solution step 4 - Test the inner func from outside the closure

--- exercises/closure-db-api-solution.js
+++ exercises/closure-db-api-solution.js
@@ -9,4 +9,18 @@ const init = () => {
     return create;
 };
 
-console.log(init()());
+const createInDb = init();
+
+console.log('\nAfter calling the inner function once, the return value is:');
+console.log(createInDb('everybooody'));
+
+
+console.log('\nAfter calling the inner function a 2nd time, the return value is:');
+console.log(createInDb('backstreet\'s'));
+
+
+console.log('\nAfter calling the inner function a 3rd time, the return value is:');
+console.log(createInDb('back'));
+
+console.log('\nAfter calling the inner function a 4th time, the return value is:');
+console.log(createInDb('alright'));
