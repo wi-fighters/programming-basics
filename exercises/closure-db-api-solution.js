4490ca4909583c4331524a0405feb4811554e807
Eva Larumbe <evalarumbe@gmail.com>
2019-09-14 10:25:07 +0200

    Closure solution step 2.4 - Test the same hack against the reverted codebase

--- exercises/closure-db-api-solution.js
+++ exercises/closure-db-api-solution.js
@@ -1,25 +1,23 @@
 const init = () => {
-    const db = [];
-    const api = [];
+  const db = [];
+  const api = [];
 
-    const create = (newElement) => {
-        db.push(newElement);
-        return db.length;
-    };
+  const create = (newElement) => {
+      db.push(newElement);
+      return db.length;
+  };
 
-    const read = () => {
-        // Use the spread operator to return a copy of the array.
-        return [...db];
+  const read = () => {
+      // Ooooh ok so this was bad!
+      // return db;
+      // What happens if we try the same hack
+      // after returning a copy?
+      return [...db];
+  };
 
-        // If we did this instead...
-        // return db;
-        // ...then the original db would be modifiable from the outer execution context,
-        // which would mean the db is no longer encapsulated (private)
-    };
+  api.push(create, read);
 
-    api.push(create, read);
-
-    return api;
+  return api;
 };
 
 // get the API
@@ -29,22 +27,63 @@ const dbApi = init();
 const createInDb = dbApi[0];
 const readDb = dbApi[1];
 
-console.log('\nAfter calling create once, the return value is:');
+console.log('\nWrite some things to db and read the contents each time...');
+console.log('No surprises so far...');
 console.log(createInDb('everybooody'));
-console.log('At this point, the DB contains:');
 console.log(readDb());
 
-console.log('\nAfter calling create a 2nd time, the return value is:');
 console.log(createInDb('backstreet\'s'));
-console.log('At this point, the DB contains:');
 console.log(readDb());
 
-console.log('\nAfter calling create a 3rd time, the return value is:');
 console.log(createInDb('back'));
-console.log('At this point, the DB contains:');
 console.log(readDb());
 
-console.log('\nAfter calling create a 4th time, the return value is:');
 console.log(createInDb('alright'));
-console.log('At this point, the DB contains:');
 console.log(readDb());
+
+console.log("\nBut I wonder if there's another way in...");
+
+// store a reference to the original db
+const stolenDb = readDb();
+
+// ... wipe the whole thing! :o
+stolenDb.length = 0;
+
+console.log("\nWhat's the first thing in the original db now?");
+console.log(readDb()[0]);
+
+console.log("\nWait, what?? Let's look at the whole db");
+console.log(readDb());
+
+// Storing it in a variable is optional.
+// We could also do this:
+console.log("\nThis time we can't use read function to write things!");
+readDb()[0] = `
+                            8b.
+                            888b
+                            8888b
+                            88888b
+                            888888b
+                          .d88888888b.__
+                    _.od888888888888888888boo..._
+                .od8888888888888888888888888888888booo.._
+             .od888888888888888888888888888888888888888888)
+           .d888P'    "Y88888888888888888888(_   _ )888P"'
+         .d8888Poo.     \`Y88888888888888888P-.\`-\`@'.-'""-.
+.oooooood8888888P""'      \`"Y8888888P.d888P   \`---'"""".-'
+d888888888'888P'  _...-----..__      'd88888__...------'
+\`""Y88888'8888|-'              \`----'|88888P
+      \`Y8888b                       Y888P
+       888888b                       \`"'
+        Y88888
+ jgs/VK  \`Y88P
+           YP
+`;
+
+console.log("\nWhat's in the original db now?");
+console.log(readDb()[0]);
+console.log(readDb()[1]);
+console.log(readDb()[2]);
+console.log(readDb()[3]);
+
+console.log("\nPhew! Much better.");
