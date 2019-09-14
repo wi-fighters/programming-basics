bd7e33401eb4e46c63936743c1a5501641b71087
Eva Larumbe <evalarumbe@gmail.com>
2019-09-14 13:46:25 +0200

    Closure solution step 4 - add a 'remove' func

--- exercises/closure-db-api-solution.js
+++ exercises/closure-db-api-solution.js
@@ -22,7 +22,18 @@ const init = () => {
     return oldElement;
   };
 
-  api.push(create, read, update);
+  const remove = (index) => {
+    const oldElement = db[index];
+    if (typeof oldElement === 'undefined') {
+      return 'Error: could not update because there was no element at the specified index. Please try a different index.';
+    }
+    // delete one element at the given index
+    db.splice(index, 1);
+    
+    return oldElement;
+  };
+
+  api.push(create, read, update, remove);
 
   return api;
 };
@@ -31,9 +42,10 @@ const init = () => {
 const dbApi = init();
 
 // extract each func from the API and give it a meaningful name
-const createInDb = dbApi[0];
-const readDb = dbApi[1];
-const updateInDb = dbApi[2];
+const createInDb =    dbApi[0];
+const readDb =        dbApi[1];
+const updateInDb =    dbApi[2];
+const removeFromDb =  dbApi[3];
 
 console.log('\nWrite some things to db and read the contents each time...');
 console.log(createInDb('everybooody'));
@@ -61,4 +73,25 @@ console.log(readDb());
 
 // Test yourself: Why does this line print undefined?
 console.log(updateInDb(4, 'yeah yeah yeah'));
+console.log(readDb());
+
+console.log('\nUpdate and delete some things, and read the db contents each time...');
+
+console.log(updateInDb(0, "I'm blue"));
+console.log(readDb());
+
+console.log(updateInDb(1, "Da ba dee"));
+console.log(readDb());
+
+console.log(updateInDb(2, "Da ba daa"));
+console.log(readDb());
+
+console.log(removeFromDb(3));
+console.log(readDb());
+
+// Test yourself: Why does this line print an error message?
+console.log(removeFromDb(4));
+console.log(readDb());
+
+console.log(removeFromDb(3));
 console.log(readDb());
